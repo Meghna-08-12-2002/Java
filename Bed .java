4c7acecc2b1f41df0cb9fda3d7eab18a4ad6f9c9
Meghna G Ninganur <ninganurmeghna@gmail.com>
2025-06-15 11:46:13 +0530

    Bed .java

--- Bed .java
+++ Bed .java
@@ -8,3 +8,9 @@ class Bed1 {
         System.out.println("Creating a Bed1 instance");  
     }  
 }  
+/* Key Concepts Used
+Class Declaration – Bed1 is a user-defined class representing a real-world object.
+
+Instance Variables – Fields like type, size, material, hasStorage store object-specific data.
+
+Constructor (No-Argument) – A default constructor is defined explicitly to print a message when an object is created*/
